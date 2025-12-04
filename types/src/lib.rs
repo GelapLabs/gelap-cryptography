ea3a4c241d4d0246052b0542affea483d7d5408e
alibarmonster <alibar838@gmail.com>
2025-12-04 15:07:10 +0700

    Implement ProofData structure with public inputs and export proof module

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,9 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod commitment;
+pub mod proof;
 pub mod signature;
 pub mod stealth;
 
-// pub mod proof;
 // pub mod transaction;
 // pub mod wallet;
