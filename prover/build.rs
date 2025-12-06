e928256080b88e20be1cb2b5e441ec576d9ce8d5
alibarmonster <alibar838@gmail.com>
2025-12-06 18:32:08 +0700

    Update build script to configure custom getrandom backend for zkVM

--- prover/build.rs
+++ prover/build.rs
@@ -1,1 +1,9 @@
+use sp1_build::{build_program_with_args, BuildArgs};
 
+fn main() {
+    let args = BuildArgs {
+        rustflags: vec!["--cfg".to_string(), "getrandom_backend=\"custom\"".to_string()],
+        ..Default::default()
+    };
+    build_program_with_args("../zkvm", args);
+}
